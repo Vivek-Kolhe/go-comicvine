d0ad66e1d9a7d4c7267fb8a2ba2199c9cd2caa75
Vivek Kolhe <vivekkolhe694@gmail.com>
2025-10-05 02:43:11 +0530

    utils for loading fixtures and mockserver

---
+++ testutil/testutil.go
@@ -0,0 +1,40 @@
+package testutil
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	gocomicvine "github.com/Vivek-Kolhe/go-comicvine"
+)
+
+type Table struct {
+	Name        string
+	FixtureFile string
+	Endpoint    string
+	TestFunc    func(client *gocomicvine.Client, t *testing.T)
+}
+
+func LoadFixture(t *testing.T, filename string) []byte {
+	t.Helper()
+	data, err := os.ReadFile(fmt.Sprintf("testdata/%s.json", filename))
+	if err != nil {
+		t.Fatalf("%s", err.Error())
+	}
+
+	return data
+}
+
+func NewMockServer(t *testing.T, fixture []byte, expectedPath string) *httptest.Server {
+	t.Helper()
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "" || expectedPath != r.URL.Path {
+			t.Fatalf("unexpected path: %s", r.URL.Path)
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(fixture)
+	}))
+}
